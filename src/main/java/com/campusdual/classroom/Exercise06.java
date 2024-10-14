5f6b5ea4f7cfce0b3081e678d41ec48e8d633210
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-14 16:21:13 +0200

    Ejercicio06

--- src/main/java/com/campusdual/classroom/Exercise06.java
+++ src/main/java/com/campusdual/classroom/Exercise06.java
@@ -17,45 +17,45 @@ public class Exercise06 {
     // Si la hora es desde las 14:00 a las 20:59, imprimir por pantalla → It's afternoon
     // Si la hora es desde las 21:00 a las 23:59, imprimir por pantalla → It's night
     public static void checkHour(int hour) {
-        {
-            switch (hour) {
-                case 21:
-                case 22:
-                case 23:
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6: {
 
-                }
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-                case 13: {
-
-
-                }
-                case 14:
-                case 15:
-                case 16:
-                case 17:
-                case 18:
-                case 19:
-                case 20: {
-
-
-                }
-                default:
-                    throw new IllegalArgumentException("Unexpected value: " + hour);
-            }
+        switch (hour) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("It's night");
+                break;
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+                System.out.println("It's morning");
+                break;
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+                System.out.println("It's afternoon");
+                break;
+            case 21:
+            case 22:
+            case 23:
+                System.out.println("It's night");
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + hour);
         }
     }
+
 
     //TODO ↓
     // Si es Enero, Febrero o Marzo, imprimir por pantalla → Winter
@@ -66,31 +66,26 @@ public class Exercise06 {
         switch (month) {
             case 1:
             case 2:
-            case 3: {
-
-
-            }
+            case 3:
+                System.out.println("Winter");
+                break;
             case 4:
             case 5:
-            case 6: {
-
-
-            }
+            case 6:
+                System.out.println("Spring");
+                break;
             case 7:
             case 8:
-            case 9: {
-
-
-            }
+            case 9:
+                System.out.println("Summer");
+                break;
             case 10:
             case 11:
-            case 12: {
-
-
-            }
+            case 12:
+                System.out.println("Autumn");
+                break;
             default:
                 throw new IllegalArgumentException("Unexpected value: " + month);
         }
     }
-
 }
